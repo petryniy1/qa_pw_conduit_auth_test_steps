242a86dadecf6126d8b31e2f03b32805a1030757
Anton Sialitski <selitskiy.anton@gmail.com>
2026-07-05 16:45:59 +0200

    add test.step to page classes wrap page methods with test.step update HomePage and SignUpPage with test.step

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import { expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 export class HomePage {
   constructor(page) {
@@ -7,6 +7,8 @@ export class HomePage {
   }
 
   async assertYourFeedTabIsVisible() {
-    await expect(this.yourFeedTab).toBeVisible();
+    await test.step(`Assert that feed tab is visible`, async () => {
+      await expect(this.yourFeedTab).toBeVisible();
+    });
   }
 }
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,4 +1,4 @@
-import { expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 export class SignUpPage {
   constructor(page) {
@@ -11,26 +11,38 @@ export class SignUpPage {
   }
 
   async open() {
-    await this.page.goto('/user/register');
+    await test.step(`Open 'Sign Up' page`, async () => {
+      await this.page.goto('/user/register');
+    });
   }
 
   async fillUsernameField(username) {
-    await this.usernameField.fill(username);
+    await test.step(`Fill the 'Username' field`, async () => {
+      await this.usernameField.fill(username);
+    });
   }
 
   async fillEmailField(email) {
-    await this.emailField.fill(email);
+    await test.step(`Fill the 'Email' field`, async () => {
+      await this.emailField.fill(email);
+    });
   }
 
   async fillPasswordField(password) {
-    await this.passwordField.fill(password);
+    await test.step('Fill the "Password" field', async () => {
+      await this.passwordField.fill(password);
+    });
   }
 
   async clickSignUpButton() {
-    await this.signUpButton.click();
+    await test.step(`Click the 'Sign up' button`, async () => {
+      await this.signUpButton.click();
+    });
   }
 
   async assertErrorMessageContainsText(messageText) {
-    await expect(this.errorMessage).toContainText(messageText);
+    await test.step(`Assert the '${messageText}' error is shown`, async () => {
+      await expect(this.errorMessage).toContainText(messageText);
+    });
   }
 }
